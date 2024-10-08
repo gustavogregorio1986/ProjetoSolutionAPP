a79268a26886e95c540ca656bb24afc58ccfa482
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-08 15:52:29 -0300

    desnevolver rotas com angfular no frontend

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
