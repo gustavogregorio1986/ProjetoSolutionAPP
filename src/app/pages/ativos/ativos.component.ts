b1168bde01eb108c0448b95ed3c458a5f2e106af
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-08 16:38:58 -0300

    desnevolvimento do forntend com angular completo, agra falta alimentar com os dados vindo do backend no C# .net 6

--- src/app/pages/ativos/ativos.component.ts
+++ src/app/pages/ativos/ativos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-ativos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './ativos.component.html',
   styleUrl: './ativos.component.css'
 })
--- src/app/pages/inativos/inativos.component.ts
+++ src/app/pages/inativos/inativos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-inativos',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './inativos.component.html',
   styleUrl: './inativos.component.css'
 })
