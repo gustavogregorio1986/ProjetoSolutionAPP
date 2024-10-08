e5631796fde5925a06a4401df574ff54cf82986d
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-08 15:45:58 -0300

    desenvolvimento inicial do projeto frontend com anuglar

---
+++ src/app/pages/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
