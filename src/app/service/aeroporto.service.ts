a896aee7f8fb9ce3a46df47f3108a5a694be461a
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-08 16:52:21 -0300

    desnevolvendo a parte do service

--- src/app/service/aeroporto.service.ts
+++ src/app/service/aeroporto.service.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AeroportoService {
 
-  private apiUrl = 'https://seuapi.com/api/aeroportos'; 
+  private apiUrl = 'https://localhost:7094/api/aeroportos'; 
 
   constructor(private http: HttpClient) { }
 
-
+  getAeroportos(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/aeroportos`);
+  }
 }
